dafe1e14bae27841fa65da1f5279cd22c921edfe
Weboscopy <weboscopy@gmail.com>
2023-11-01 13:11:23 +0100

    tuto 08 sur les types littéraux et les énumérateurs

--- index.ts
+++ index.ts
@@ -1,77 +1,130 @@
-type User = {
-    userId: number
-    username: string
-    // propriété image optionnelle <=> union string | undefined
-    img ?: string
-    location ? : {
-        address? : string
+// Constante
+const PORT  = 5000 
+let port = 3000
+
+type PORT = 5000
+
+// Types littéraux 
+function emitEvent(name: "Login", data: {userId: number}) : void
+function emitEvent(name: "SendMessage", data: {message: string}) : void
+function emitEvent(name: string, data: unknown){
+    console.log(`on${name}: ${JSON.stringify(data)}`)
+}
+
+
+const checkStatus = (status: "pending" | "success"  ) : string => {
+    if(status === "pending"){
+        return "En cours de traitement"
+    }
+
+    if(status === "success" ){
+        return "Votre demande a été acceptée"
+    }
+
+    throw new Error("Erreur ")
+}
+
+checkStatus("pending")
+
+// Enumérateur numérique
+const httpRequest = (status: number) => {
+    switch(status){
+        case 0:
+            return "Succès"
+        case 1: 
+            return "Redirection"
+        case 3:
+            return "Mauvaise requête"
+        case 4:
+            return "Erreur serveur"
+        default:
+            return "Temps écoulé"
     }
 }
 
-const getUserImg = ( user : User ) : string => {
-    // if(user.img){
-    //    return user.img
-    // }
+// Union de type littéraux
+type status = "success" | "redirect" | "failed" | "error"
 
-    // return "defaultImg.jpg"
-
-    return user.img ? user.img : "defaultimg.jpg"
-}
-
-function getAddress(user: User) : string {
-    return user?.location?.address ?? ""
-}
-
-// Paramètre optionnel dans une fonction
-const order = (amount : number, price: number, discount ?: number ): string => {
-    return `Montant de votre commande : ${discount ? amount * price - discount : amount * price}`
-}
-
-type Coord = {x: number, y : number}
-
-// fonction callback optionnelle 
-function translate (coord: Coord, cb?:(coord: Coord) => void)  {
-    coord.x += 10 
-    coord.y += 10 
-
-    cb?.(coord)
-}
-
-
-const coord : Coord= {x : 0, y :0}
-
-translate(coord, (coord) => console.log(coord))
-
-class CustomDate {
-    day: number = 1
-    month: number = 1
-    year: number = 2000
-    constructor(day: number, month : number, year: number){
-        this.day = day 
-        this.month = month 
-        this.year = year
+const httpRequest2 = (status: status) : string => {
+    switch(status){
+        case "success":
+            return "succès !"
+        case "redirect":
+            return "redirection..."
+        case "failed":
+            return  "mauvaise requête"
+        case "error":
+            return "erreur serveur"
     }
 }
 
-// garde de type
-const getFormatedDate = ( date : number | Date | CustomDate ) : string => {
+const failedReq = (type : "forbidden" | "unauthorized") => {
+    return type === "forbidden" ? 401 : 403
+ }
 
-    // on utilise typeof pour les primitives
-    if(typeof date === "number"){
-        return new Date(date).toLocaleString()
-    }
-
-    // on utilise instanceof pour les structures
-    if(date instanceof Date){
-        return date.toLocaleString()
-    }
-
-    if(date instanceof CustomDate){
-        return `${date.day} - ${date.month} - ${date.year}`
-    }
-
-    throw new Error("Erreur format date")
+ // Enumération dynamique
+enum Status {
+    success =  200,
+    redirect = 300,
+    failed = failedReq("forbidden"),
+    error = 500,
 }
 
+const httpRequest3 = (status: Status) : string => {
+    switch(status){
+        case 200:
+            return "En attente"
+        case 300: 
+            return "Succès !"
+        case 400:
+            return "Mauvaise requête"
+        case 500:
+            return "Erreur serveur !"
+        default: 
+            return "temps écoulé"
+    }
+}
+
+console.log(httpRequest3(3))
+
+// Enumérateur de chaînes de caractères 
+enum PrimaryColor {
+    red = "#FF0000",
+    green = "#00FF00",
+    blue = "#0000FF",
+}
+
+enum Color  {
+    primary = "#FFF",
+    secondary = "#000",
+    tertiary = "#eee",
+    red = PrimaryColor.red
+}
+
+const darkTheme = {
+    color: Color.primary,
+    backgroundColor: Color.secondary,
+    borderColor: Color.tertiary
+}
+
+const lightTheme = {
+    color: Color.secondary,
+    backgroundColor: Color.primary,
+    borderColor: Color.red
+}
+
+
+const getColorName = (color: Color)  : string => {
+    switch(color){
+        case Color.primary:
+            return "black"
+        case Color.secondary:
+            return "white"
+        case Color.tertiary:
+            return "gray"
+        default: 
+            return "black"
+    }
+}
 
 
