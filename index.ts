83efbdc2837f307d5e08783e67f630707915f824
Weboscopy <weboscopy@gmail.com>
2023-12-19 12:42:49 +0100

    tuto 18 sur les mixins

--- index.ts
+++ index.ts
@@ -1,159 +1,97 @@
-interface User {
-    name: string
-    createPost(): void
-}
-
-interface Admin extends User {
-    name: string
-    deletePost(): void
-}
-
- 
-type Example1 = Admin extends User ? string : number 
-
-type Example2 = Admin extends Date ? string : number 
-
-// Fonction Curry 
-const signUp = <T extends {name: string}> (user : T) => {
-    console.log(user.name)
-    return  (input : T extends Admin ? string : number ) => {
-            console.log(input)
-    }
-
-} 
-
-const user : User = {
-    name: "Paul",
-    createPost(){
+class HorrorMovie  {
+    scary : boolean = true
+    scare(){
+        console.log("ce film fait peur")
     }
 }
 
-const admin : Admin = {
-    name: "Admin",
-    createPost(){},
-    deletePost(){}
+
+class ComedyMovie  {
+    funny : boolean = true
+    
+    makeLaugh(){
+        console.log(`ce film fait rire`)
+    }
 }
 
-const createUserPassword = signUp(user)
-createUserPassword(1033030)
+// class HorrorComedyMovie extends Movie, Horror {
 
-const createAdminPassword = signUp(admin)
-createAdminPassword("jzizrofifizfize")
+// }
 
 
-// Type utilitaire personnalisé  
+type Class = new (...args : any[])=> any; 
 
-type Music = {
-    fileName: string,
-    time: number,
-    type: "mp3"
-    rewind() : void 
-    play(): void 
-}
-
-type Video = {
-    fileName: string 
-    time: number
-    type: "mp4"
-    rewind() : void 
-    play(): void 
-}
-
-type Pdf = {
-    fileName: string 
-    type: "pdf"
-    download(): void 
-}
-
-type Media = Music | Video | Pdf 
-type Playable <T> = T extends {time: number, play : () => void } ? T : never 
-
-function replay (file: Playable<Media>){
-    file.rewind()
-    file.play()
-}
-
-replay({
-    fileName : "music",
-    type: "mp4",
-    time: 400,
-    rewind(){
-        this.time = 0
-    },
-    play(){
-        while(this.time < 10){
-            this.time++
+const ComedyMovieMixin =  <T extends Class> (baseClass : T) => {
+    return class extends baseClass {
+        funny : boolean = true
+    
+        makeLaugh(){
+            console.log(`ce film fait rire`)
         }
     }
-})
-
-
-
-
-// Callback optionnel 
-
-interface Post {
-    id: number,
-    userId: number,
-    title: string,
-    body: string
 }
 
-const url = "http://jsonplaceholder.typicode.com/posts"
-
-
-type Result<T> = T extends undefined ? Promise<Post[]> : void 
-
-
-const fetchPosts = <T extends undefined | ((data: Post[]) => void) > (
-    cb?: T
-) : Result<T> => {
-    if(cb) {
-        fetch(url)
-            .then(res => res.json())
-            .then(cb)
-            return undefined as Result<T> 
-    } else {
-        return fetch(url)
-                .then(res => res.json()) as Result<T>
-                
+const HorrorMovieMixin =  <T extends Class> (baseClass : T) => {
+    return class extends baseClass {
+        scary : boolean = true
+        scare(){
+            console.log("ce film fait peur")
+        }
     }
 }
 
 
-(async () => {
-    const data = await fetchPosts() as Post[]
-    data.forEach(post => console.log(post.title)) 
-})();
+const HorrorComedyMovie = ComedyMovieMixin(HorrorMovieMixin(
+    class {}
+))
+
+const film = new HorrorComedyMovie()
+
+
+
+const MiniDBMixin= <T extends Class, U> (baseClass: T) => {
+    return class extends baseClass {
+        protected db : Record<string, U> = {}
     
+        set(key: string, val: U): void {
+            this.db[key] = val 
+        }
 
-fetchPosts((data) => {
-    data.forEach(post => console.log(post.title))
-})
+        get(): Record<string, U> {
+            return this.db
+        }
+    }
+}
 
-// Alternative en utilisant la surcharge de fonction
+const PersistantDBMixin = <T extends Class, U> (baseClass: T) => {
+    return class extends baseClass {
+        protected db : Record<string, U> = {}
+        save() : string {
+           return  JSON.stringify(this.db)
+        }
+    
+        load(str : string) : void {
+            this.db = JSON.parse(str)
+        }
+    }
+}
 
-// function fetchPosts2(
-//     cb?: (data: Post[]) => void 
-// ) :  void 
+let DB = PersistantDBMixin(MiniDBMixin(class {}))
 
-// function fetchPosts2() : Promise<Post[]>
+const myDB = new DB()
 
-// function fetchPosts2(
-//     cb?: (data: Post[]) => void 
-// ) : unknown {
-//     if(cb){
-//         fetch(url)
-//             .then(res => res.json())
-//             .then(cb)
-//             return undefined
-//     } else {
-//         return fetch(url)
-//                     .then(res => res.json())
-//     }
-// }
+myDB.set("user", {username: "paul"})
+myDB.set("post", {title: "Article 1"})
+myDB.set("comment", {comment: "hey !"})
 
+console.log(myDB.get())
 
-// function createError(msg: string) : never {
-//     throw new Error(msg)
-// }
+const saved = myDB.save()
+
+myDB.set("user", {username: "tom"})
+
+console.log(myDB.get())
+
+myDB.load(saved)
+
+console.log(myDB.get())
